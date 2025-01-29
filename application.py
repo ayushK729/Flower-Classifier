5507d453e646b4e9f228c7fb575a41e4cc6f64f4
Ayush <ayushkumar76523@gmail.com>
2025-01-29 17:07:51 +0530

    final build

--- application.py
+++ application.py
@@ -4,6 +4,11 @@ import streamlit as st
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_iris
 
+import warnings
+# Suppress only DeprecationWarnings from Streamlit
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
 st.set_page_config(
     page_title="Flower Classifier",
     page_icon="res/ico.png"
@@ -25,7 +30,7 @@ model=RandomForestClassifier()
 model.fit(df.iloc[:,:-1],df['species'])
 
 # gathering input from user using streamlit
-sepal_len=st.sidebar.slider("Sepal Length",float(df['sepal length (cm)'].min()),float(df['sepal length (cm)'].max()))
+sepal_len=st.sidebar.slider("Sepal Length",float(df['sepal length (cm)'].min()),float(df['sepal length (cm)'].max()),)
 sepal_wid=st.sidebar.slider("Sepal Width",float(df['sepal width (cm)'].min()),float(df['sepal width (cm)'].max()))
 petal_len=st.sidebar.slider("Petal Length",float(df['petal length (cm)'].min()),float(df['petal length (cm)'].max()))
 petal_wid=st.sidebar.slider("Petal Width",float(df['petal width (cm)'].min()),float(df['petal width (cm)'].max()))
@@ -38,14 +43,17 @@ prediction=model.predict(features)
 predicted_species=target_names[prediction[0]]
 
 # output of the predictin
-st.write("Prediction")
-st.write(f"Predicted Species = {predicted_species}")
+st.write("## Prediction")
+st.write(f"Predicted Species = **{predicted_species}**")
 
 
 # adding conditional flower information
 if predicted_species=="setosa":
+    st.image("res/setosa.jpg","Iris Setosa",width=400)
     st.write(f"Setosa is one of the three species of the Iris flower and is the smallest among them. It is characterized by its relatively small sepal length, sepal width, petal length, and petal width. The flowers of Setosa are typically found to have shorter petals and broader sepals compared to the other species. Setosa is often the easiest to distinguish due to its distinct and compact appearance. The flowers typically have a striking blue or purple color and a pleasant scent. It is known for its resilience in varying growing conditions and has a high adaptability rate.")
 elif predicted_species=="virginica":
+    st.image("res/virginica.jpg","Iris Virginica",width=400)
     st.write(f"Virginica is the largest of the three Iris species and is known for its long and narrow petals. The flower has a prominent and vibrant color, typically a deep violet or blue, and it has a distinct structure with a pronounced difference between the length of its petals and sepals. Virginica flowers are commonly found in wetlands, particularly in areas along the eastern United States. These flowers tend to have a more elongated and slender shape compared to Setosa and Versicolor. Known for its hardiness, Virginica is often seen in natural landscapes and is valued for its aesthetic appeal in gardens. It requires more care and attention when cultivated in non-native habitats.")
 elif predicted_species=="versicolor":
+    st.image("res/versicolor.jpeg","Iris Versicolor",width=400)
     st.write(f"Versicolor is a hybrid species that exhibits characteristics between Setosa and Virginica. Its flowers typically have a medium sepal length and width, and its petals are larger than Setosa's but smaller than Virginica's. Versicolor’s petals are a mix of blue and purple hues, giving it a unique appearance. It tends to be less compact than Setosa, and its blooms are a bit more elongated. The plant thrives in various soil types and is widely cultivated for ornamental purposes. Versicolor is often described as a “middle ground” species due to its intermediate size and characteristics.")
